d3b4e8082d1584105b749fb247b6b2310fc4f8ff
RuslanAgishev <agishrus@felk.cvut.cz>
2023-09-24 11:28:17 +0200

    updated data creation scripts: increased job running time, train: batch size, added regularization

--- scripts/data/create_terrain_features.sh
+++ scripts/data/create_terrain_features.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
 
 DATA_PATH=/home/$USER/data/robingas/data
-ROBOT_NAME='marv'
-EXPERIMENT_NAME='22-08-12-cimicky_haj'
+#ROBOT_NAME='marv'
+#EXPERIMENT_NAME='22-08-12-cimicky_haj'
 #SEQUENCE_NAME='ugv_2022-08-12-15-18-34'
-SEQUENCE_NAME='ugv_2022-08-12-16-37-03'
+#SEQUENCE_NAME='ugv_2022-08-12-16-37-03'
 
-source /home/agishrus/workspace/traversability_ws/devel/setup.bash
-./create_terrain_features --dataset-path $DATA_PATH/$EXPERIMENT_NAME/$ROBOT_NAME/${SEQUENCE_NAME}_trav/ \
+ROBOT_NAME='husky'
+EXPERIMENT_NAME='22-09-27-unhost'/${ROBOT_NAME}
+#EXPERIMENT_NAME='22-10-27-unhost-final-demo'
+#SEQUENCE_NAME='husky_2022-09-27-15-01-44'
+SEQUENCE_NAME='husky_2022-09-27-10-33-15'
+#SEQUENCE_NAME='husky_2022-10-27-15-33-57'
+
+source /home/$USER/workspaces/traversability_ws/devel/setup.bash
+./create_terrain_features --dataset-path $DATA_PATH/$EXPERIMENT_NAME/${SEQUENCE_NAME}_trav/ \
                           --control-model diffdrive \
                           --visualize False --save-data True
--- scripts/train_monoforce.sh
+++ scripts/train_monoforce.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source /home/$USER/workspaces/traversability_ws/devel/setup.bash
-./train_monoforce --batch_size 48
+./train_monoforce --batch_size 64 --regularization 0.2
 
