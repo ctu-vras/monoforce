03f1612c2c5248ce51dbee81b9364140e423dd09
RuslanAgishev <agishrus@fel.cvut.cz>
2023-11-20 15:34:13 +0100

    cleaned up, publishing several sampled paths in ros monoforce node

--- src/monoforce/ros.py
+++ src/monoforce/ros.py
@@ -2,9 +2,10 @@ import torch
 import numpy as np
 from scipy.ndimage import rotate
 from tqdm import tqdm
+from jsk_recognition_msgs.msg import BoundingBox
 from std_msgs.msg import Header
 from nav_msgs.msg import Path
-from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
+from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion, PoseArray, TransformStamped
 from tf.transformations import quaternion_from_matrix, euler_from_quaternion, quaternion_matrix
 from std_msgs.msg import Float32MultiArray, MultiArrayDimension
 from grid_map_msgs.msg import GridMap
@@ -14,27 +15,8 @@ from numpy.lib.recfunctions import unstructured_to_structured
 from tf2_ros import BufferCore
 from rosbag import Bag, ROSBagException
 import rospy
+from visualization_msgs.msg import Marker
 
-
-def xyzR_to_pose_msg(xyz, R):
-    T = np.eye(4)
-    T[:3, :3] = R
-    xyz = xyz.squeeze()
-    msg = Pose(Point(*xyz), Quaternion(*quaternion_from_matrix(T)))
-    return msg
-
-def traj_to_path_msg(pos_x, pos_R):
-    # assert isinstance(pos_x, torch.Tensor) and isinstance(pos_R, torch.Tensor)
-    N = len(pos_x)
-    assert pos_x.shape == (N, 3, 1)
-    assert pos_R.shape == (N, 3, 3)
-    if isinstance(pos_x, torch.Tensor):
-        pos_x = pos_x.detach().cpu().numpy()
-    if isinstance(pos_R, torch.Tensor):
-        pos_R = pos_R.detach().cpu().numpy()
-    msg = Path()
-    msg.poses = [PoseStamped(Header(), xyzR_to_pose_msg(xyz, R)) for xyz, R in zip(pos_x, pos_R)]
-    return msg
 
 def height_map_to_gridmap_msg(heightmap, grid_res,
                               xyz=np.array([0, 0, 0]), q=np.array([0., 0., 0., 1.])):
@@ -120,3 +102,95 @@ def load_tf_buffer(bag_paths, tf_topics=None):
             print('Could not read %s: %s' % (path, ex))
 
     return tf_buffer
+
+
+def to_tf(pose, frame_id, child_frame_id, stamp=None):
+    assert pose.shape == (4, 4)
+    if stamp is None:
+        stamp = rospy.Time.now()
+
+    t = TransformStamped()
+    t.header.stamp = stamp
+    t.header.frame_id = frame_id
+    t.child_frame_id = child_frame_id
+    t.transform.translation.x = pose[0, 3]
+    t.transform.translation.y = pose[1, 3]
+    t.transform.translation.z = pose[2, 3]
+    q = quaternion_from_matrix(pose)
+    t.transform.rotation.x = q[0]
+    t.transform.rotation.y = q[1]
+    t.transform.rotation.z = q[2]
+    t.transform.rotation.w = q[3]
+    return t
+
+def to_cloud_msg(cloud, stamp=None, frame_id=None, fields=None):
+    assert isinstance(cloud, np.ndarray)
+    assert cloud.shape[1] >= 3
+    if fields is None:
+        fields = ['x', 'y', 'z']
+    # https://answers.ros.org/question/197309/rviz-does-not-display-pointcloud2-if-encoding-not-float32/
+    cloud = np.asarray(cloud, dtype=np.float32)
+    cloud_struct = unstructured_to_structured(cloud, names=fields)
+    return msgify(PointCloud2, cloud_struct, stamp=stamp, frame_id=frame_id)
+
+def to_pose_array(poses, stamp=None, frame_id=None):
+    assert isinstance(poses, np.ndarray) or isinstance(poses, torch.Tensor)
+    assert poses.shape[1:] == (4, 4)
+    pose_array = PoseArray()
+    pose_array.header.stamp = stamp
+    pose_array.header.frame_id = frame_id
+    for i in range(poses.shape[0]):
+        pose = msgify(Pose, poses[i])
+        pose_array.poses.append(pose)
+    return pose_array
+
+def to_path(poses, stamp=None, frame_id=None):
+    assert isinstance(poses, np.ndarray) or isinstance(poses, torch.Tensor)
+    assert poses.shape[1:] == (4, 4)
+    path = Path()
+    path.header.stamp = stamp
+    path.header.frame_id = frame_id
+    for i in range(poses.shape[0]):
+        pose = msgify(Pose, poses[i])
+        path.poses.append(pose)
+    return path
+
+def to_box_msg(pose, size, stamp=None, frame_id=None):
+    assert isinstance(pose, np.ndarray) or isinstance(pose, torch.Tensor)
+    assert pose.shape == (4, 4)
+    assert isinstance(size, np.ndarray) or isinstance(size, torch.Tensor)
+    assert size.shape == (3,)
+    box = BoundingBox()
+    box.header.stamp = stamp
+    box.header.frame_id = frame_id
+    box.pose.position.x = pose[0, 3]
+    box.pose.position.y = pose[1, 3]
+    box.pose.position.z = pose[2, 3]
+    q = quaternion_from_matrix(pose)
+    box.pose.orientation.x = q[0]
+    box.pose.orientation.y = q[1]
+    box.pose.orientation.z = q[2]
+    box.pose.orientation.w = q[3]
+    box.dimensions.x = size[0]
+    box.dimensions.y = size[1]
+    box.dimensions.z = size[2]
+    return box
+
+def to_marker(poses, color=None):
+    assert isinstance(poses, np.ndarray) or isinstance(poses, torch.Tensor)
+    assert poses.shape[1:] == (4, 4)
+    marker = Marker()
+    marker.type = Marker.LINE_STRIP
+    marker.action = Marker.ADD
+    marker.pose.orientation.w = 1.0
+    marker.scale.x = 0.1
+    if color is not None:
+        assert len(color) == 3
+        marker.color.r = color[0]
+        marker.color.g = color[1]
+        marker.color.b = color[2]
+    marker.color.a = 1.0
+    for t in range(poses.shape[0]):
+        p = poses[t]
+        marker.points.append(msgify(Pose, p).position)
+    return marker
