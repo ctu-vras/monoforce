c0e76e6cc906819056f7487b0217586a59c2b8e8
RuslanAgishev <agishrus@fel.cvut.cz>
2024-04-12 16:59:07 +0200

    minor changes

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,10 +3,8 @@
 source /home/$USER/workspaces/traversability_ws/devel/setup.bash
 
 WEIGHTS=/home/agishrus/workspaces/traversability_ws/src/monoforce/config/tb_runs/lss_rellis3d_2024_03_06_16_07_52/train_lss.pt
-./train --bsz 32 --nworkers 12 --nepochs 1000 --lr 0.001 --weight_decay 1e-7 \
-        --map_consistency False \
+./train --bsz 4 --nworkers 12 --nepochs 1000 --lr 0.001 --weight_decay 1e-7 \
         --debug False --vis False \
-        --lidar_hm_weight 1.0 --traj_hm_weight 100.0 --hdiff_weight 0.001 \
-	--pretrained_model_path ${WEIGHTS} \
-	--dataset robingas
-
+        --geom_hm_weight 1.0 --terrain_hm_weight 100.0 --hdiff_weight 0.001 \
+      	--dataset robingas
+      	#--pretrained_model_path ${WEIGHTS} \
