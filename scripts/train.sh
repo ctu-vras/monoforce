98d0d1c83e4e625d7633309402ed114ecac34720
RuslanAgishev <agishrus@fel.cvut.cz>
2024-04-25 09:57:27 +0200

    added robot argument for lss trainer script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/config/weights/lss/lss_rellis3d.pt
+ROBOT=husky
+DATASET=robingas
+
 source /home/$USER/workspaces/traversability_ws/devel/setup.bash
 
-WEIGHTS=/home/agishrus/workspaces/traversability_ws/src/monoforce/config/tb_runs/lss_robingas_2024_04_13_19_26_27/lss.pt
-
 ./train --bsz 32 --nworkers 12 --nepochs 1000 --lr 0.001 --weight_decay 1e-7 \
-        --debug False --vis False \
+        --debug True --vis True \
         --geom_hm_weight 1.0 --terrain_hm_weight 10.0 --hdiff_weight 1e-4 \
-        --dataset robingas \
-	      --pretrained_model_path ${WEIGHTS}
+        --dataset $DATASET \
+        --robot $ROBOT \
+        --dphys_cfg_path ../config/dphys_cfg.yaml \
+        --lss_cfg_path ../config/lss_cfg_$ROBOT.yaml
+#	      --pretrained_model_path ${WEIGHTS}
