8e20d2bf870e849ecafd124f2a31c34012db71ee
RuslanAgishev <agishrus@felk.cvut.cz>
2024-04-25 17:47:28 +0200

    lss training script: with oru husky data

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/config/weights/lss/lss_rellis3d.pt
-ROBOT=husky
+WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/config/tb_runs/lss_robingas_husky_oru_2024_04_25_10_38_43/lss.pt
+ROBOT=husky_oru
 DATASET=robingas
 
 source /home/$USER/workspaces/traversability_ws/devel/setup.bash
 
-./train --bsz 32 --nworkers 12 --nepochs 1000 --lr 0.001 --weight_decay 1e-7 \
-        --debug True --vis True \
+./train --bsz 64 --nworkers 2 --nepochs 1000 --lr 0.001 --weight_decay 1e-7 \
+        --debug False --vis False \
         --geom_hm_weight 1.0 --terrain_hm_weight 10.0 --hdiff_weight 1e-4 \
         --dataset $DATASET \
         --robot $ROBOT \
         --dphys_cfg_path ../config/dphys_cfg.yaml \
-        --lss_cfg_path ../config/lss_cfg_$ROBOT.yaml
-#	      --pretrained_model_path ${WEIGHTS}
+        --lss_cfg_path ../config/lss_cfg_$ROBOT.yaml \
+	--only_front_hm True \
+        --pretrained_model_path ${WEIGHTS}
