78d5dd1024933bbeb2785d30a950085de45e13ce
RuslanAgishev <agishrus@fel.cvut.cz>
2024-10-09 11:15:25 +0200

    towards same terrain encoder model for all robots with basler rgb camera

--- monoforce/scripts/train.sh
+++ monoforce/scripts/train.sh
@@ -7,15 +7,14 @@ DEBUG=False
 VIS=False
 BSZ=32
 
-WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/config/weights/lss/lss_${DATASET}_${ROBOT}.pt
+WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/config/weights/lss/lss.pt
 
 source $HOME/workspaces/traversability_ws/devel/setup.bash
 
-./train --bsz $BSZ --nworkers 16 --nepochs 1000 --lr 1e-4 --weight_decay 1e-7 \
+./train --bsz $BSZ --nworkers 16 --nepochs 1000 --lr 1e-3 --weight_decay 1e-7 \
         --debug $DEBUG --vis $VIS \
         --geom_hm_weight 1.0 --terrain_hm_weight 10.0 --hdiff_weight 1e-6 --phys_weight 10.0 \
         --dataset $DATASET \
         --robot $ROBOT \
-        --lss_cfg_path ../config/lss_cfg_$ROBOT.yaml \
         --only_front_cam ${ONLY_FRONT_CAM} \
         --pretrained_model_path ${WEIGHTS}
