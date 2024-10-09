6cd4e5ebfc3389b236d7f1981e8fcfb9c2a03a86
RuslanAgishev <agishrus@fel.cvut.cz>
2024-10-09 11:15:25 +0200

    towards same terrain encoder model for all robots with basler rgb camera

--- monoforce/scripts/train_friction.sh
+++ monoforce/scripts/train_friction.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-ROBOT=tradr
+ROBOT=tradr2
 DATASET=robingas
 DEBUG=False
 VIS=False
 BSZ=128
 
-WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/config/weights/lss/lss_${DATASET}_${ROBOT}.pt
+WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/config/weights/lss/lss.pt
 
 source $HOME/workspaces/traversability_ws/devel/setup.bash
 
-./train_friction --bsz $BSZ --nepochs 1000 --lr 1e-5 \
+./train_friction --bsz $BSZ --nepochs 1000 --lr 1e-4 \
                  --debug $DEBUG --vis $VIS \
                  --dataset $DATASET \
                  --robot $ROBOT \
-                 --lss_cfg_path ../config/lss_cfg_$ROBOT.yaml \
                  --pretrained_model_path ${WEIGHTS}
