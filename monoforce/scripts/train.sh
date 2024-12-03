e2527397a40d36b4562f5be3345ffcce681e0d8c
RuslanAgishev <agishrus@fel.cvut.cz>
2024-12-03 16:51:13 +0100

    training hyperparams upd

--- monoforce/scripts/train.sh
+++ monoforce/scripts/train.sh
@@ -8,9 +8,9 @@ BSZ=32  # 32, 32, 8
 WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/config/weights/${MODEL}/val.pth
 
 source $HOME/workspaces/traversability_ws/devel/setup.bash
-./train.py --bsz $BSZ --nepochs 1000 --lr 1e-4 --weight_decay 1e-7 \
+./train.py --bsz $BSZ --nepochs 1000 --lr 1e-3 --weight_decay 1e-7 \
            --debug $DEBUG --vis $VIS \
-           --geom_weight 1.0 --terrain_weight 2.0 --phys_weight 0.1 \
+           --geom_weight 1.0 --terrain_weight 2.0 --phys_weight 1.0 \
            --robot $ROBOT \
            --model $MODEL \
            --pretrained_model_path ${WEIGHTS}
