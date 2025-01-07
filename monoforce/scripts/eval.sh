0e2890299e07fca7980cf2c946e32213deb7bd91
RuslanAgishev <agishrus@fel.cvut.cz>
2025-01-07 17:54:49 +0100

    evaluating different terrain encoders and traj predictors

--- monoforce/scripts/eval.sh
+++ monoforce/scripts/eval.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-TERRAIN_ENCODER=voxelnet  # lss, voxelnet, bevfusion
-TRAJ_PREDICTOR=traj_lstm  # dphysics, traj_lstm
-WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/config/weights/${TERRAIN_ENCODER}/val.pth
-ROBOT=marv
-
+echo "Source ROS workspace..."
 source $HOME/workspaces/traversability_ws/devel/setup.bash
 
-echo "Evaluating model ${TERRAIN_ENCODER}..."
-./eval.py --terrain_encoder ${TERRAIN_ENCODER} \
-          --terrain_encoder_path ${WEIGHTS} \
-          --traj_predictor ${TRAJ_PREDICTOR} \
-          --robot ${ROBOT} \
-          --vis
+ROBOT=marv  # marv, tradr, husky
+TERRAIN_ENCODERS=(lss voxelnet bevfusion)
+TRAJ_PREDICTORS=(dphysics traj_lstm)
+
+for TERRAIN_ENCODER in "${TERRAIN_ENCODERS[@]}"
+do
+  for TRAJ_PREDICTOR in "${TRAJ_PREDICTORS[@]}"
+  do
+    WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/config/weights/${TERRAIN_ENCODER}/val.pth
+    echo "Evaluating terrain encoder ${TERRAIN_ENCODER} with trajectory predictor ${TRAJ_PREDICTOR}..."
+    ./eval.py --terrain_encoder ${TERRAIN_ENCODER} \
+              --terrain_encoder_path ${WEIGHTS} \
+              --traj_predictor ${TRAJ_PREDICTOR} \
+              --robot ${ROBOT} #--vis
+  done
+done
+
 echo "Done evaluating."
