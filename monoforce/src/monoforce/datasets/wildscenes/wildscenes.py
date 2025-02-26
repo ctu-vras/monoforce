7f1d7ac00ede25e2bb5259fdc44f7138783e42e7
RuslanAgishev <agishrus@fel.cvut.cz>
2025-02-26 17:16:28 +0100

    wildscenes dataloader: terrain encoder input camera data, geom heightmap

--- monoforce/src/monoforce/datasets/wildscenes/wildscenes.py
+++ monoforce/src/monoforce/datasets/wildscenes/wildscenes.py
@@ -2,23 +2,19 @@ import copy
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
+from monoforce.cloudproc import estimate_heightmap
 from monoforce.models.terrain_encoder.utils import sample_augmentation, img_transform, normalize_img
 from monoforce.utils import read_yaml
-from .utils3d import load_pcd
+from monoforce.models.traj_predictor.dphys_config import DPhysConfig
 from .utils import (timestamp_to_bag_time,
                     get_ids_2d, get_ids_3d,
                     convert_ts_to_float,
                     get_extrinsics_yaml,
-                    get_intrinsics,
-                    viz_image,
-                    read_rgb_image)
+                    get_intrinsics)
 from ..rough import data_dir
 import os
-import open3d as o3d
 import numpy as np
 import quaternion
-import cv2
-import matplotlib.pyplot as plt
 import pandas as pd
 from glob import glob
 from pathlib import Path
@@ -29,6 +25,11 @@ class BaseDataset(Dataset):
         super().__init__()
         self.ids = []
         self.is_train = is_train
+        self.camera_names = ['camera_front']
+        self.dphys_cfg = DPhysConfig()
+        self.lss_cfg = read_yaml(os.path.join(data_dir, '../config', 'lss_cfg_wildscenes.yaml'))
+        self.grid_res = self.lss_cfg['grid_conf']['xbound'][2]
+        self.calib = {}
 
     def get_sample(self, i):
         raise NotImplementedError
@@ -53,63 +54,32 @@ class BaseDataset(Dataset):
     def __len__(self):
         return len(self.ids)
 
-
-class WildScenes(BaseDataset):
-    """
-    https://csiro-robotics.github.io/WildScenes/
-    """
-
-    def __init__(self, seq, is_train=False):
-        super().__init__(is_train=is_train)
-        self.seq = seq
-        self.cloud_paths = sorted(glob(os.path.join(data_dir, f'WildScenes/WildScenes3d/{seq}', 'Clouds', '*')))
-        self.cloud_label_paths = sorted(glob(os.path.join(data_dir, f'WildScenes/WildScenes3d/{seq}', 'Labels', '*')))
-        self.img_paths = sorted(glob(os.path.join(data_dir, f'WildScenes/WildScenes2d/{seq}', 'image', '*')))
-        self.img_label_paths = sorted(glob(os.path.join(data_dir, f'WildScenes/WildScenes2d/{seq}', 'label', '*')))
-        self.ids, self.cloud_ts = self.get_cloud_ids()
-        _, self.img_ts = self.get_image_ids()
-        self.calib = self.get_calib()
-        self.camera_names = ['camera_front']
-        self.lss_cfg = read_yaml(os.path.join(data_dir, '../config', 'lss_cfg_wildscenes.yaml'))
-
-    def get_cloud_ids(self):
-        cloud_seq_path = os.path.join(data_dir, f'WildScenes/WildScenes3d/{self.seq}')
-        cloud_timestamp_strings = [
-            timestamp_to_bag_time(t) for t in get_ids_3d(Path(cloud_seq_path))
-        ]
-        cloud_timestamp_strings = sorted(cloud_timestamp_strings)
-        cloud_timestamps = convert_ts_to_float(cloud_timestamp_strings)
-        return cloud_timestamp_strings, cloud_timestamps
-
-    def get_image_ids(self):
-        image_seq_path = os.path.join(data_dir, f'WildScenes/WildScenes2d/{self.seq}')
-        image_timestamp_strings = [
-            timestamp_to_bag_time(t) for t in get_ids_2d(Path(image_seq_path))
-        ]
-        image_timestamp_strings = sorted(image_timestamp_strings)
-        image_timestamps = convert_ts_to_float(image_timestamp_strings)
-        return image_timestamp_strings, image_timestamps
-
     def get_cloud(self, i):
-        cloud_path = self.cloud_paths[i]
-        cloud = np.fromfile(cloud_path, dtype=np.float32).reshape(-1, 3)
-        return cloud
+        raise NotImplementedError
 
     def get_image(self, i):
-        img_path = self.img_paths[i]
-        img = Image.open(img_path)
-        return img
+        raise NotImplementedError
 
-    def get_calib(self):
-        path = os.path.join(data_dir, f'WildScenes/WildScenes2d/{self.seq}')
-        params = read_yaml(os.path.join(path, 'camera_calibration.yaml'))['centre-camera']
-        K, D = get_intrinsics(params['intrinsics'])
-        E = get_extrinsics_yaml(params['extrinsics'])
-        calib = {'K': K,
-                 'D': D,
-                 'E': E,
-                 'imgW': 2016, 'imgH': 1512}
-        return calib
+    def get_geom_height_map(self, i):
+        """
+        Get height map from lidar point cloud.
+        :param i: index of the sample
+        :param cached: if True, load height map from file if it exists, otherwise estimate it
+        :param dir_name: directory to save/load heightmap
+        :return: heightmap (2 x H x W), where 2 is the number of channels (z and mask)
+        """
+        points = torch.as_tensor(self.get_cloud(i))
+        lidar_hm = estimate_heightmap(points, d_max=self.dphys_cfg.d_max,
+                                      grid_res=self.grid_res,
+                                      h_max=self.dphys_cfg.h_max,
+                                      r_min=self.dphys_cfg.r_min)
+        heightmap = torch.as_tensor(lidar_hm)
+        return heightmap
+
+    def get_terrain_height_map(self, i):
+        # TODO: use semantic classes to estimate terrain height map
+        heightmap = self.get_geom_height_map(i)
+        return heightmap
 
     def get_images_data(self, i):
         imgs = []
@@ -163,171 +133,125 @@ class WildScenes(BaseDataset):
 
         return img_data
 
+
+class WildScenes(BaseDataset):
+    """
+    https://csiro-robotics.github.io/WildScenes/
+    """
+
+    def __init__(self, seq, is_train=False):
+        super().__init__(is_train=is_train)
+        self.seq = seq
+        self.cloud_paths = sorted(glob(os.path.join(data_dir, f'WildScenes/WildScenes3d/{seq}', 'Clouds', '*')))
+        self.cloud_label_paths = sorted(glob(os.path.join(data_dir, f'WildScenes/WildScenes3d/{seq}', 'Labels', '*')))
+        self.img_paths = sorted(glob(os.path.join(data_dir, f'WildScenes/WildScenes2d/{seq}', 'image', '*')))
+        self.img_label_paths = sorted(glob(os.path.join(data_dir, f'WildScenes/WildScenes2d/{seq}', 'label', '*')))
+        _, self.cloud_ts = self.get_cloud_ids()
+        self.ids, self.img_ts = self.get_image_ids()
+        self.calib = self.get_calib()
+        self.poses = self.get_poses()
+
+    def get_cloud_ids(self):
+        cloud_seq_path = os.path.join(data_dir, f'WildScenes/WildScenes3d/{self.seq}')
+        cloud_timestamp_strings = [
+            timestamp_to_bag_time(t) for t in get_ids_3d(Path(cloud_seq_path))
+        ]
+        cloud_timestamp_strings = sorted(cloud_timestamp_strings)
+        cloud_timestamps = convert_ts_to_float(cloud_timestamp_strings)
+        return cloud_timestamp_strings, cloud_timestamps
+
+    def get_image_ids(self):
+        image_seq_path = os.path.join(data_dir, f'WildScenes/WildScenes2d/{self.seq}')
+        image_timestamp_strings = [
+            timestamp_to_bag_time(t) for t in get_ids_2d(Path(image_seq_path))
+        ]
+        image_timestamp_strings = sorted(image_timestamp_strings)
+        image_timestamps = convert_ts_to_float(image_timestamp_strings)
+        return image_timestamp_strings, image_timestamps
+
+    def get_cloud(self, i):
+        img_i = self.get_cloud_i_closest_to_image(i)
+        cloud_path = self.cloud_paths[img_i]
+        cloud = np.fromfile(cloud_path, dtype=np.float32).reshape(-1, 3)
+        return cloud
+
+    def get_image(self, i):
+        img_path = self.img_paths[i]
+        img = Image.open(img_path)
+        return img
+
+    def get_cloud_i_closest_to_image(self, i):
+        img_ts = self.img_ts[i]
+        cloud_ts = np.asarray(self.cloud_ts)
+        img_ts = np.asarray(img_ts)
+        diff = np.abs(cloud_ts - img_ts)
+        bestcloud_i = np.argmin(diff)
+        bestdiff = diff[bestcloud_i]
+        if bestdiff > 5:
+            print(f"Warning: For this image idx there is no suitable point cloud anywhere near this image")
+            # raise ValueError("For this image idx there is no suitable point cloud anywhere near this image")
+        return bestcloud_i
+
+    def get_calib(self):
+        path = os.path.join(data_dir, f'WildScenes/WildScenes2d/{self.seq}')
+        params = read_yaml(os.path.join(path, 'camera_calibration.yaml'))['centre-camera']
+        K, D = get_intrinsics(params['intrinsics'])
+        E = get_extrinsics_yaml(params['extrinsics'])
+        calib = {'K': K,
+                 'D': D,
+                 'E': E,
+                 'imgW': 2016, 'imgH': 1512}
+        return calib
+
+    def get_poses(self):
+        image_seq_path = os.path.join(data_dir, f'WildScenes/WildScenes2d/{self.seq}')
+        df = pd.read_csv(os.path.join(image_seq_path, 'poses2d.csv'), sep=' ').sort_index()
+        xyz_q_cam = np.array(df[['x', 'y', 'z', 'qx', 'qy', 'qz', 'qw']])
+        R_cam = quaternion.as_rotation_matrix(quaternion.from_float_array(xyz_q_cam[:, 3:]))
+        poses_cam = np.zeros((len(xyz_q_cam), 4, 4))
+        poses_cam[:, :3, :3] = R_cam
+        poses_cam[:, :3, 3] = xyz_q_cam[:, :3]
+        poses_cam[:, 3, 3] = 1.0
+
+        E = self.calib['E']
+        poses = poses_cam @ E.T
+
+        return poses
+
+    def get_pose(self, i):
+        return self.poses[i]
+
     def get_sample(self, i):
         imgs, rots, trans, intrins, post_rots, post_trans = self.get_images_data(i)
-        traj_ts, states = self.get_states_traj(i)
-        Xs, Xds, Rs, Omegas = states
         hm_geom = self.get_geom_height_map(i)
         hm_terrain = self.get_terrain_height_map(i)
-        pose0 = torch.as_tensor(self.get_initial_pose_on_heightmap(i), dtype=torch.float32)
         return (imgs, rots, trans, intrins, post_rots, post_trans,
-                hm_geom, hm_terrain,
-                pose0,
-                traj_ts, Xs, Xds, Rs, Omegas)
-
-
-def project_labelcloud_to_image(
-    cloud_wrt_camera, cameraparams, rgb_img, label_img, image_name, vizoutfolder=None
-):
-    # assumes the points are in the local coordinate frame of the camera, not the global coordinate frame
-
-    intrinsics_K, intrinsics_D = get_intrinsics(
-        cameraparams["centre-camera"]["intrinsics"]
-    )
-    imgpoints, _ = cv2.projectPoints(
-        cloud_wrt_camera[:, :3].astype(np.float64),
-        np.zeros((1, 3)),
-        np.zeros((1, 3)),
-        intrinsics_K,
-        np.zeros(5),
-    )
-
-    imgW, imgH = 2016, 1512
-
-    depth_map = np.ones((imgW, imgH)) * np.inf
-    dcount = 0
-    for p_id, p in enumerate(imgpoints):
-        # x, y = int(p[0][0]), int(p[0][1])
-        x, y = int(imgpoints[p_id][0][0]), int(imgpoints[p_id][0][1])
-        d = cloud_wrt_camera[p_id][2]
-        if (x < 0) or (x >= imgW) or (y < 0) or (y >= imgH) or (d <= 1):  # or (d >= 30)
-            continue
-
-        # d = point_depths[p_id]
-        if d >= depth_map[x, y]:
-            continue
-        depth_map[x, y] = d
-        dcount += 1
-        drawcolor = (255, 0, 0)
-
-        rgb_img = cv2.circle(rgb_img, (x, y), radius=2, color=drawcolor, thickness=-1)
-
-    plt.imshow(rgb_img)
-
-    if vizoutfolder is not None:
-        viz_image(rgb_img, vizpath=os.path.join(vizoutfolder, "lidarproject_" + image_name))
-
-    return dcount
-
-def project_cloud_to_image():
-    import matplotlib as mpl
-    mpl.use('TkAgg')
-
-    loaddir = os.path.join(data_dir, "WildScenes/WildScenes3d/K-01")
-
-    cloud_xyz = sorted(glob(os.path.join(loaddir, 'Clouds', '*')))
-    labels = sorted(glob(os.path.join(loaddir, 'Labels', '*')))
-
-    twopath = loaddir.replace('WildScenes3d', 'WildScenes2d')
-    images = sorted(glob(os.path.join(twopath, 'image', '*')))
-    labelimages = sorted(glob(os.path.join(twopath, 'label', '*')))
-
-    image_timestamp_strings = [
-        timestamp_to_bag_time(t) for t in get_ids_2d(Path(twopath))
-    ]
-    cloud_timestamp_strings = [
-        timestamp_to_bag_time(t) for t in get_ids_3d(Path(loaddir))
-    ]
-    image_timestamp_strings = sorted(image_timestamp_strings)
-    cloud_timestamp_strings = sorted(cloud_timestamp_strings)
-
-    image_timestamps = convert_ts_to_float(image_timestamp_strings)
-    cloud_timestamps = convert_ts_to_float(cloud_timestamp_strings)
-
-    idx = 200  # temp
-    thistimestamp = image_timestamps[idx]
-
-    # search for nearest cloud for this image:
-    bestdiff = 1e10
-    bestcloudidx = -1
-    for cloudidx, cloudts in enumerate(cloud_timestamps):
-        tdiff = np.abs(thistimestamp - cloudts)
-        if tdiff < bestdiff:
-            bestdiff = tdiff
-            bestcloudidx = cloudidx
-    if bestdiff > 5:
-        raise ValueError("For this image idx there is no suitable point cloud anywhere near this image")
-
-    pcd = load_pcd(cloud_xyz[bestcloudidx], labels[bestcloudidx])
-    o3d.visualization.draw_geometries([pcd])
-
-    # load intrinsics and extrinsics
-    cameraparams = read_yaml(os.path.join(twopath, 'camera_calibration.yaml'))
-
-    # get extrinsics
-    camextdata = cameraparams['centre-camera']['extrinsics']
-
-    extT = get_extrinsics_yaml(camextdata)
-
-    # load pose information
-    poses2d = pd.read_csv(os.path.join(twopath, 'poses2d.csv'), sep=' ').sort_index()
-    poses2d = poses2d.rename(columns={poses2d.columns[0]: "ts"})
-    poses2d = poses2d.set_index('ts')
-
-    image_timestamps_datetime = pd.to_datetime(
-        [float(ts) for ts in image_timestamp_strings], unit="s"
-    )
-
-    this2dpose = poses2d.loc[str(image_timestamps_datetime[idx])]
-
-    q = quaternion.quaternion(this2dpose.qw, this2dpose.qx, this2dpose.qy, this2dpose.qz)
-    Rm = quaternion.as_rotation_matrix(q)
-    T = np.eye(4)
-    T[:3, :3] = Rm
-    T[:3, 3] = np.array([this2dpose.x, this2dpose.y, this2dpose.z])
-
-    cloud_wrt_camera = pcd.transform(np.linalg.inv(extT))  # this is the lidar points in the local reference frame.
-
-    # If want to convert points into a global ref frame:
-    # test = cloud_wrt_camera.transform(T)
-
-    viewcloud = np.asarray(cloud_wrt_camera.points)
-
-    # pcd_transformed = pcd.transform(global2local)
-    # viewcloud = np.asarray(pcd_transformed.points)
-    # cloud_wrt_camera = pcd.transform(np.linalg.inv(T))
-    # viewcloud = np.asarray(cloud_wrt_camera.points)
-
-    # load raw image and labelimg
-    raw_rgb_img = read_rgb_image(images[idx])
-    label_img = read_rgb_image(labelimages[idx])
-
-    print('')
-
-    successcount = project_labelcloud_to_image(
-        viewcloud.astype(np.float64),
-        cameraparams,
-        raw_rgb_img,
-        label_img,
-        image_name=image_timestamp_strings[idx],
-        vizoutfolder=None,
-    )
-    print("Number of lidar points projected: ", successcount)
-
-    plt.show()
+                hm_geom, hm_terrain)
 
 
 def main():
-    project_cloud_to_image()
-    # ds = WildScenes(seq='K-01')
-    # print(len(ds))
-    # idx = 0
-    # points = ds.get_cloud(idx)
-    # img = ds.get_image(idx)
-    # print(points.shape)
-    # print(np.asarray(img).shape)
-    # imgs, rots, trans, intrins, post_rots, post_trans = ds.get_images_data(idx)
-    # print(imgs.shape)
+    from monoforce.utils import explore_data
+    import open3d as o3d
+    import matplotlib.pyplot as plt
+    from tqdm import tqdm
+    import matplotlib as mpl
+    mpl.use('TkAgg')
+
+    seq = np.random.choice(['K-01', 'K-03', 'V-01', 'V-02', 'V-03'])
+    ds = WildScenes(seq=seq, is_train=True)
+
+    # poses = ds.poses[:10]
+    # plt.figure()
+    # X = poses[:, 0, 3]
+    # Y = poses[:, 1, 3]
+    # plt.plot(X, Y, '.')
+    # plt.show()
+
+    idx = np.random.randint(len(ds))
+    explore_data(ds, sample_range=[idx])
+
+    # for i in tqdm(range(len(ds))):
+    #     sample = ds[i]
 
 
 if __name__ == "__main__":
