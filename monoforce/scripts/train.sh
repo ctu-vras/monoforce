df40da5fd385d9a37cd8c28d2920094ce4dca898
rlnav <agishrus@fel.cvut.cz>
2025-05-06 09:47:33 +0200

    fix in covariance estimation: network ouputs actual log(variance)

--- monoforce/scripts/train.sh
+++ monoforce/scripts/train.sh
@@ -9,5 +9,5 @@ WEIGHTS=$HOME/workspaces/ros2/traversability_ws/src/monoforce/monoforce/config/w
 source $HOME/workspaces/ros2/traversability_ws/install/setup.bash
 python train.py --batch_size $BSZ --n_epochs 1000 --lr 1e-3 \
                 --debug $DEBUG --vis $VIS \
-                --geom_weight 1.0 --terrain_weight 2.0 --phys_weight 0.0 --logvar_weight 1.0 \
+                --geom_weight 1.0 --terrain_weight 0.0 --phys_weight 0.0 --logvar_weight 2.0 \
                 --pretrained_terrain_encoder_path ${WEIGHTS}
